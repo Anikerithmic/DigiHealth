7759d9c258247e4960078b967025af13a3373118
Anikerithmic <aniketkakde55@gmail.com>
2025-02-08 11:41:10 +0530

    :sparkles: Feat: Update related files

--- DigiHealth/src/App.jsx
+++ DigiHealth/src/App.jsx
@@ -2,13 +2,62 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import PatientForm from "./pages/patient-form-page";
+import AdminDashboard from "./pages/admin-dashboard-page";
+import AppointmentSuccess from "./components/Appointment/AppointmentSuccess";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       {/* <Navbar /> */}
+//       <PatientForm />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/HomeDashboard";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+// import About from './components/Home/About';
+import AboutPage from "./components/Home/AboutPage";
+import Contact from "./components/Home/Contact";
+import Patient from "./components/Patient/Patient";
+// import Doctor from './components/Doctor/Doctor';
+// import AdminDashboard from "./components/Admin/AdminDashboard";
+import PatientDashboard from "./components/Patient/PatientDashboard";
+import RegisterPatient from "./components/Patient/PatientRegistrationForm";
+import DoctorDashboard from "./components/Doctor/DoctorDashboard";
+import RegisterDoctor from "./components/Doctor/DoctorRegistrationForm";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pages/patient-form-page" element={<PatientForm />} />
+        <Route path="/pages/admin-dashboard-page" element={<AdminDashboard />} />
+        <Route path="/appointment-success" element={<AppointmentSuccess />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register-patient" element={<RegisterPatient />} />
+        <Route path="/register-doctor" element={<RegisterDoctor />} />
+        <Route path="/patient" element={<Patient />} />
+        {/* <Route path="/doctor" element={<Doctor />} /> */}
 
-  return;
-  <></>;
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- DigiHealth/tailwind.config.js
+++ DigiHealth/tailwind.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
+        colors: {
+            'dark-gray': '#131619', // Define custom color
+        },
     },
     plugins: [],
-}
+};
